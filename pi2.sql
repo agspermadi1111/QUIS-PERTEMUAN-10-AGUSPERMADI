1c0b4f24a10d88adcf0b13d4aa4d526acbba8952
Agus Permadi <agsperr@gmail.com>
2021-06-30 20:18:05 +0700

    COMMIT QUIS

---
+++ pi2.sql
@@ -0,0 +1,117 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 30 Jun 2021 pada 15.16
+-- Versi server: 10.4.18-MariaDB
+-- Versi PHP: 7.4.16
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pi2`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `member`
+--
+
+CREATE TABLE `member` (
+  `member_id` int(11) NOT NULL,
+  `nama` varchar(100) CHARACTER SET latin1 DEFAULT NULL,
+  `alamat` varchar(100) CHARACTER SET latin1 DEFAULT NULL,
+  `tlp` int(15) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `member`
+--
+
+INSERT INTO `member` (`member_id`, `nama`, `alamat`, `tlp`) VALUES
+(23, 'Agus', 'klayan', 2147483647),
+(26, 'Ridwan', 'Mayung', 2147483647);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `mobil`
+--
+
+CREATE TABLE `mobil` (
+  `mobil_id` int(11) NOT NULL,
+  `merktipe` varchar(100) CHARACTER SET latin1 DEFAULT NULL,
+  `thnkeluaran` varchar(10) CHARACTER SET latin1 DEFAULT NULL,
+  `tarif` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `mobil`
+--
+
+INSERT INTO `mobil` (`mobil_id`, `merktipe`, `thnkeluaran`, `tarif`) VALUES
+(23, 'Toyota', '2019', 2147483647),
+(24, 'Alphard', '2020', 200000000);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `rental`
+--
+
+CREATE TABLE `rental` (
+  `rental_id` int(11) NOT NULL,
+  `mobil_id` int(11) NOT NULL,
+  `member_id` int(11) NOT NULL,
+  `lamarental` varchar(100) CHARACTER SET latin1 DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `member`
+--
+ALTER TABLE `member`
+  ADD PRIMARY KEY (`member_id`);
+
+--
+-- Indeks untuk tabel `mobil`
+--
+ALTER TABLE `mobil`
+  ADD PRIMARY KEY (`mobil_id`);
+
+--
+-- Indeks untuk tabel `rental`
+--
+ALTER TABLE `rental`
+  ADD PRIMARY KEY (`rental_id`),
+  ADD KEY `mobil_id` (`mobil_id`),
+  ADD KEY `member_id` (`member_id`);
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `rental`
+--
+ALTER TABLE `rental`
+  ADD CONSTRAINT `rental_ibfk_1` FOREIGN KEY (`member_id`) REFERENCES `member` (`member_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `rental_ibfk_2` FOREIGN KEY (`mobil_id`) REFERENCES `mobil` (`mobil_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
